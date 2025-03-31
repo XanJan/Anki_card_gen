4641c299af72fe5108f66cbf25449ab56817b4dd
Anna Majberger <annamajberger@gmail.com>
2025-03-31 18:46:59 +0200

    Added a label

--- GUI.py
+++ GUI.py
@@ -1,9 +1,23 @@
+import tkinter as tk
 import customtkinter as ctk
 
 root = ctk.CTk()
+width = 600
+height = 600
 
-def initMainWindow(root, width, height):
-    root.geometry(str(width) + 'x' + str(height))
-    root.mainloop()
+root.geometry(str(width) + 'x' + str(height))
+lable = ctk.CTkLabel(root, text="Anki card generator",            
+                     width=120, 
+                height=50, 
+                font=('Roboto', 12))
+#print(ctk._fonts)
 
-initMainWindow(root, 600, 600)
+lable.place(relx=0.5, rely=0.1, anchor=tk.CENTER)
+
+def button_callback():
+    print("button clicked")
+
+button = ctk.CTkButton(root, text="my button", command=button_callback)
+button.pack(padx=20, pady=70)
+
+root.mainloop()
